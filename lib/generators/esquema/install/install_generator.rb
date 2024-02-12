fa068695ffd90766718d57e3ce290e56accf59fa
Sergio Bayona <sbayona@martianinteractive.com>
2024-02-12 17:10:08 -0500

    remove extra stuff from install generator

--- lib/generators/esquema/install/install_generator.rb
+++ lib/generators/esquema/install/install_generator.rb
@@ -10,23 +10,6 @@ module Esquema
       def copy_initializer_file
         template "esquema_initializer.rb", "config/initializers/esquema.rb"
       end
-
-      def add_rspec
-        # Add specific actions here, like copying files, adding lines to files, etc.
-        # For example, to add RSpec to the Gemfile:
-        gem_group :development, :test do
-          gem "rspec-rails", "~> x.x.x"
-        end
-        # Don't forget to run bundle install if you modify the Gemfile
-      end
-
-      private
-
-      def gem_group(*names)
-        append_to_file "Gemfile", "\n\ngroup #{names.map(&:inspect).join(", ")} do\n", verbose: true
-        yield if block_given?
-        append_to_file "Gemfile", "end\n", verbose: true
-      end
     end
   end
 end
