9c758bf3ad4b1cefc457144c11acce9514ffc6eb
Sergio Bayona <sbayona@martianinteractive.com>
2024-02-13 20:18:53 -0500

    spec fix

--- spec/cases/multiple_objects_spec.rb
+++ spec/cases/multiple_objects_spec.rb
@@ -1,88 +1,64 @@
 # frozen_string_literal: true
 
-RSpec.describe "A schema with multiple objects" do
-  module Foo
-    class User < ActiveRecord::Base
-      has_many :tasks, class_name: "Foo::Task"
+require "spec_helper"
 
-      include Esquema::Model
-    end
-
-    class Task < ActiveRecord::Base
-      belongs_to :user, class_name: "Foo::User"
-    end
+module Multi
+  class User < ActiveRecord::Base
+    include Esquema::Model
+    has_many :tasks, class_name: "Multi::Task"
   end
 
-  subject(:model) { Foo::User }
+  class Task < ActiveRecord::Base
+    belongs_to :user, class_name: "Multi::User"
+  end
+end
 
+RSpec.describe "A schema with multiple objects" do
   it "includes the associated model schema" do
-    expect(model.json_schema).to include_json({
-                                                "title": "User",
-                                                "type": "object",
-                                                "properties": {
-                                                  "id": {
-                                                    "type": "integer",
-                                                    "title": "Id"
-                                                  },
-                                                  "name": {
-                                                    "type": "string",
-                                                    "title": "Name"
-                                                  },
-                                                  "email": {
-                                                    "type": "string",
-                                                    "title": "Email"
-                                                  },
-                                                  "group": {
-                                                    "type": "integer",
-                                                    "title": "Group"
-                                                  },
-                                                  "dob": {
-                                                    "type": "date",
-                                                    "title": "Dob"
-                                                  },
-                                                  "salary": {
-                                                    "type": "number",
-                                                    "title": "Salary"
-                                                  },
-                                                  "active": {
-                                                    "type": "boolean",
-                                                    "default": false,
-                                                    "title": "Active"
-                                                  },
-                                                  "bio": {
-                                                    "type": "string",
-                                                    "title": "Bio"
-                                                  },
-                                                  "country": {
-                                                    "type": "string",
-                                                    "default": "United States of America",
-                                                    "title": "Country"
-                                                  },
-                                                  "preferences": {
-                                                    "title": "Preferences"
-                                                  },
-                                                  "created_at": {
-                                                    "type": "date-time",
-                                                    "title": "Created at"
-                                                  },
-                                                  "updated_at": {
-                                                    "type": "date-time",
-                                                    "title": "Updated at"
-                                                  },
-                                                  "tasks": {
-                                                    "type": "array",
-                                                    "title": "Tasks",
-                                                    "items": {
-                                                      "title": "Task",
+    expect(Multi::User.json_schema).to include_json({
+                                                      "title": "User",
                                                       "type": "object",
                                                       "properties": {
                                                         "id": {
                                                           "type": "integer",
                                                           "title": "Id"
                                                         },
-                                                        "title": {
+                                                        "name": {
                                                           "type": "string",
-                                                          "title": "Title"
+                                                          "title": "Name"
+                                                        },
+                                                        "email": {
+                                                          "type": "string",
+                                                          "title": "Email"
+                                                        },
+                                                        "group": {
+                                                          "type": "integer",
+                                                          "title": "Group"
+                                                        },
+                                                        "dob": {
+                                                          "type": "date",
+                                                          "title": "Dob"
+                                                        },
+                                                        "salary": {
+                                                          "type": "number",
+                                                          "title": "Salary"
+                                                        },
+                                                        "active": {
+                                                          "type": "boolean",
+                                                          "default": false,
+                                                          "title": "Active"
+                                                        },
+                                                        "bio": {
+                                                          "type": "string",
+                                                          "title": "Bio"
+                                                        },
+                                                        "country": {
+                                                          "type": "string",
+                                                          "default": "United States of America",
+                                                          "title": "Country"
+                                                        },
+                                                        "preferences": {
+                                                          "title": "Preferences"
                                                         },
                                                         "created_at": {
                                                           "type": "date-time",
@@ -91,31 +67,54 @@ RSpec.describe "A schema with multiple objects" do
                                                         "updated_at": {
                                                           "type": "date-time",
                                                           "title": "Updated at"
+                                                        },
+                                                        "tasks": {
+                                                          "type": "array",
+                                                          "title": "Tasks",
+                                                          "items": {
+                                                            "title": "Task",
+                                                            "type": "object",
+                                                            "properties": {
+                                                              "id": {
+                                                                "type": "integer",
+                                                                "title": "Id"
+                                                              },
+                                                              "title": {
+                                                                "type": "string",
+                                                                "title": "Title"
+                                                              },
+                                                              "created_at": {
+                                                                "type": "date-time",
+                                                                "title": "Created at"
+                                                              },
+                                                              "updated_at": {
+                                                                "type": "date-time",
+                                                                "title": "Updated at"
+                                                              }
+                                                            },
+                                                            "required": %w[
+                                                              id
+                                                              title
+                                                              created_at
+                                                              updated_at
+                                                            ]
+                                                          }
                                                         }
                                                       },
                                                       "required": %w[
                                                         id
-                                                        title
+                                                        name
+                                                        email
+                                                        group
+                                                        dob
+                                                        salary
+                                                        active
+                                                        bio
+                                                        country
+                                                        preferences
                                                         created_at
                                                         updated_at
                                                       ]
-                                                    }
-                                                  }
-                                                },
-                                                "required": %w[
-                                                  id
-                                                  name
-                                                  email
-                                                  group
-                                                  dob
-                                                  salary
-                                                  active
-                                                  bio
-                                                  country
-                                                  preferences
-                                                  created_at
-                                                  updated_at
-                                                ]
-                                              })
+                                                    })
   end
 end
